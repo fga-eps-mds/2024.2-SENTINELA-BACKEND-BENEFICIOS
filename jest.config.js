9464f1b2e173df5463385e04139a9df6051cc87c
saracampss <saracampxx@gmail.com>
2024-08-23 16:29:22 -0300

    [fix:3] - fix timeout value
    Signed-off-by: saracampss <saracampxx@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@
 
 const reporter = process.env.JEST_SONAR;
 const config = {
+    testTimeout: 30000,
     // All imported modules in your tests should be mocked automatically
     // automock: false,
 
