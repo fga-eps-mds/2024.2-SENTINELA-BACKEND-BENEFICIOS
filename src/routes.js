e1ed0a86e5fbf3cb58c77dfb965a350e270c5ccf
Clara Ribeiro <claramarcelinors@gmail.com>
2025-01-19 15:36:15 -0300

    fix: ajusta arquivos

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const routes = express.Router();
 const benefitsFormController = require("./Controllers/benefitsFormController");
-const {checkPermissions} = require("./Middlewares/accessControlMiddleware");
+const { checkPermissions } = require("./Middlewares/accessControlMiddleware");
 
-routes.post("/benefits/create",checkPermissions('beneficios_criar'), benefitsFormController.createBenefitsForm);
-routes.get("/benefits", checkPermissions('beneficios_visualizar'), benefitsFormController.getBenefitsForm);
-routes.get("/benefits/:id", checkPermissions('beneficios_visualizar'), benefitsFormController.getBenefitsFormById);
+routes.post(
+    "/benefits/create",
+    checkPermissions("beneficios_criar"),
+    benefitsFormController.createBenefitsForm
+);
+routes.get(
+    "/benefits",
+    checkPermissions("beneficios_visualizar"),
+    benefitsFormController.getBenefitsForm
+);
+routes.get(
+    "/benefits/:id",
+    checkPermissions("beneficios_visualizar"),
+    benefitsFormController.getBenefitsFormById
+);
 routes.delete(
-    "/benefits/delete/:id",checkPermissions('beneficios_deletar'),
+    "/benefits/delete/:id",
+    checkPermissions("beneficios_deletar"),
     benefitsFormController.deleteBenefitsFormById
 );
 routes.patch(
-    "/benefits/update/:id", checkPermissions('beneficios_editar'),
+    "/benefits/update/:id",
+    checkPermissions("beneficios_editar"),
     benefitsFormController.updateBenefitsFormById
 );
 module.exports = routes;
